6332800e185491222080e88d7da4ec7110267ace
joelfright <44258779+joelfright@users.noreply.github.com>
2019-01-14 16:05:45 +0000

    Switched Classes in order to merge

--- src/core/src/com/game/zombies/GameScreen.java
+++ src/core/src/com/game/zombies/GameScreen.java
@@ -7,101 +7,99 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
 
-public class GameScreen implements Screen {
+public class GameScreen extends ApplicationAdapter  implements Screen {
 
-	final ZombieGame game;
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
-	private Sprite zom;
-	private Texture zomIm;
-	public BitmapFont font;
+	ZombieGame game;
+	TiledMap tiledMap;
+	OrthographicCamera camera;
+	boolean complete = false;
+	SpriteBatch sb;
+	Texture texture;
+	Sprite player;
+	TiledMapRenderer tiledMapRenderer;
 
-	/*
-	 * Sets up GameScreen
-	 * Sets camera, batch and font,
-	 * Sets up Zombie Sprite
-	 */
-	public GameScreen(final ZombieGame game) {
-
+	public GameScreen(ZombieGame game) {
 		this.game = game;
-		//create camera + load texture
+
+	}
+
+	@Override
+	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+
+
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1280,720);
-		game.batch = new SpriteBatch();
-		game.font = new BitmapFont();
-		zomIm = new Texture("data/zomb.png");
-		zom = new Sprite(zomIm);
-		zom.setPosition(10, 10);
-	}
-
-	public void create() {
-
-	}
-
-	@Override
-	public void render(float delta) {
-		//Set screen colour
-		Gdx.gl.glClearColor(0.5f,0.25f,1,1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		// tell the camera to update its matrices.
+		camera.setToOrtho(false,w,h);
 		camera.update();
-		game.batch.setProjectionMatrix(camera.combined);
-
-		Movement();
-
-		game.batch.begin();
-		game.font.draw(game.batch, "testing", 50,50);
-		zom.draw(game.batch);
-		game.batch.end();
+		tiledMap = new TmxMapLoader().load("data/map.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
 
+		sb = new SpriteBatch();
+		texture = new Texture(Gdx.files.internal("data/player.png"));
+		player = new Sprite(texture);
+		player.setPosition(Gdx.graphics.getWidth()/2 - 32, Gdx.graphics.getHeight()/2 - 32);
 
-	}
-
-	//Checks input for zombie movement - will be moved to HumanoidClass
-	public void Movement() {
-
-		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-			zom.translateY(5f);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-			zom.translateY(-5f);
-		}
-
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			zom.translateX(-5f);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			zom.translateX(5f);
-		}
 	}
 
 	@Override
-	public void resize(int width, int height) {
+	public void render (float delta) {
+		if(complete == false){
+			create();
+			complete = true;
+		}
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+		sb.setProjectionMatrix(camera.combined);
+		sb.begin();
+
+		player.draw(sb);
+
+		sb.end();
+		camera.position.set(player.getX(), player.getY(), 0);
+		camera.update();
+		Movement();
+		camera.position.x = MathUtils.clamp(camera.position.x, 1280 / 2, 1600 - 1280 /2);
+		camera.position.y = MathUtils.clamp(camera.position.y, 720 /2, 1024 - 720 /2);
+
+	}
+
+
+	public void Movement() {
+		if (Gdx.input.isKeyPressed(Input.Keys.UP) && (player.getY() < 1024 -64)) {
+			player.translateY(2);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && (player.getY() > 0)) {
+			player.translateY(-2);
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && (player.getX() > 0)) {
+			player.translateX(-2);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && (player.getX() < 1600-64)) {
+			player.translateX(2);
+		}
 	}
 
 	@Override
 	public void show() {
+
 	}
 
 	@Override
 	public void hide() {
-	}
 
-	@Override
-	public void pause() {
 	}
-
-	@Override
-	public void resume() {
-	}
-
-	@Override
-	public void dispose() {
-		zomIm.dispose();
-		batch.dispose();
-	}
-
 }
